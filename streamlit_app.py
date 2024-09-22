4ee90aadeca55bc2636e5b1a3e658f7a0164381d
larachs <raespinoza@estudiantec.cr>
2024-09-22 15:27:02 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
 st.title("🎈 My new app")
+st.write("Hola soy Rache")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
